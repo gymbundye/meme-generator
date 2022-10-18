ee849b895ce5e7415059456d1b20769df7e6c477
gymbundye <gymbundye@gmail.com>
2022-10-18 11:15:15 -0400

    worked on header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import TrollFace from 'Images/TrollFace.png'
+import TrollFace from './Images/Troll-Face.png'
 
 function Header() {
   return (
     <div className='Header'>
-        <img>{TrollFace}</img>
+        
+        <img src= {TrollFace} alt="your mama!"  className='TrollFace'/>
+
         <h1 className='Title'>Meme Generator</h1>
+
+        <p className='header-text'>React Course-Project 3</p>
     </div>
   )
 }
