373f7d564c7b02e23599620bc1b9b0a3fb2fa05c
gymbundye <gymbundye@gmail.com>
2022-10-19 14:36:46 -0400

    started meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header'
+import Meme from './Components/Meme'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Meme/>
        
     </div>
   )
---
+++ src/Components/Meme.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Meme() {
+  return (
+    <div>
+        <form className='meme-form'>
+            <input placeholder='Shut Up'></input>
+            <input placeholder='and take my money!'></input>
+        </form>
+        <button></button>
+    </div>
+  )
+}
+
+export default Meme
