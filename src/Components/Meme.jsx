cf60b9fd74f4ae36329ad61397f0cb7443a8944b
gymbundye <gymbundye@gmail.com>
2022-10-25 23:08:16 -0400

    added usestate to change state

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import memesData from './memesData'
+
 
 function Meme() {
+
+  const [memeImage, setMemeImage] = React.useState('')
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setMemeImage(memesArray[randomNumber].url)
+}
+
+
+
   return (
     <main>
-        <form className='meme-form'>
-            <input  className='meme-input' type='text'  placeholder='Top Text'></input>
+        <div className='meme-form'>
+            <input className='meme-input' type='text' placeholder='Top Text'></input>
             <input className='meme-input' type='text' placeholder='Bottom Text'></input>
-            <button className='meme-button'>Get a new Meme Image 🖼</button>
-        </form>
+            <button onClick= {getMemeImage} className='meme-button'>Get a new Meme Image 🖼</button>
+          </div>
+          <img src= {memeImage} className ='meme-image'/>
+          
     </main>
   )
 }
